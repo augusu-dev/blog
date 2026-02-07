67964136bd3e4a4a3a8257fff23a17e9c8f5abae
augusu <kou061488+1@gmail.com>
2026-02-08 01:28:19 +0900

    Delete src directory

--- src/app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllBlogMeta } from "@/lib/markdown";
-import BlogList from "@/components/BlogList";
-
-export default function BlogPage() {
-  const articles = getAllBlogMeta();
-  return <BlogList articles={articles} />;
-}
--- src/app/learn/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllLearnMeta } from "@/lib/markdown";
-import LearnView from "@/components/LearnView";
-
-export default function LearnPage() {
-  const entries = getAllLearnMeta();
-  return <LearnView entries={entries} />;
-}
